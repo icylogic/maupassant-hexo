7d7fc67f0e2cf0f2e745fd99d9d5346065f564d5
Chris Zheng <chriszheng99@gmail.com>
2017-08-06 15:19:32 +0800

    Add support for QR code generation.
    You must have hexo-helper-qrcode installed.

--- source/js/share.js
+++ source/js/share.js
@@ -8,6 +8,7 @@
 
     var $this = $(this),
       url = $this.attr('data-url'),
+      qrcode_img = $this.attr('data-qrcode'),
       encodedUrl = encodeURIComponent(url),
       id = 'article-share-box-' + $this.attr('data-id'),
       title = document.title,
@@ -25,11 +26,11 @@
         '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://service.weibo.com/share/share.php?title=' + title + '&url=' + encodedUrl + '&searchPic=true&style=number' + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
-            '<a href="javascript:void(0);" class="article-share-wechat" target="_blank" title="Wechat"></a>',
-            '<div class="wechat-qrcode"><img src="http://s.jiathis.com/qrcode.php?url=' + encodedUrl + '"></div>',
+            '<a href="//twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="//www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="//service.weibo.com/share/share.php?title=' + title + '&url=' + encodedUrl + '&searchPic=true&style=number' + '" class="article-share-weibo" target="_blank" title="Weibo"></a>',
+            '<a href="' + qrcode_img + '" class="article-share-qrcode" target="_blank" title="QR code"></a>',
+            '<div class="qrcode"><img src=' + qrcode_img + '></div>',
           '</div>',
         '</div>'
       ].join('');
